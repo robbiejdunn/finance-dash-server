0e3631a72239eacc085775123e9d322b4ac69e19
robbiejdunn <robbiejdunn94@gmail.com>
2021-12-15 23:28:36 +0000

    deploy script improvements

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 
-# npm install backend CDK deps
-cd cdk_back && npm install
+# exit when any command fails
+set -e
 
-# npm install lambda deps
-cd lambdas
-for D in *; do
-    if [ -d "${D}" ]; then
-        cd "${D}" && npm install && cd ..
-    fi
-done
+echo "Select deploy type: f = fullstack, b = only backend, f = only frontend"
+read DEPLOY_TYPE
 
-# deploy backend stack
-cd .. && cdk deploy && cd ..
+if [[ "$DEPLOY_TYPE" == "b" ]]; then
+    # npm install backend CDK deps
+    cd cdk_back && npm install
 
-# install react app deps
-npm install
+    # npm install lambda deps
+    cd lambdas
+    for D in *; do
+        if [[ -d "${D}" ]]; then
+            cd "${D}" && npm install && cd ..
+        fi
+    done
 
-# grab backend API endpoint using AWS CLI and build react app
-API_ENDPOINT=$(
-    aws cloudformation describe-stacks \
-        --stack-name FinanceDashServerStack \
-        --query "Stacks[0].Outputs[?ExportName=='FinanceDashAPIEndpoint'].OutputValue" \
-        --output text
-)
-REACT_APP_FINANCE_DASH_API_ENDPOINT=$API_ENDPOINT npm run build
+    # deploy backend stack
+    cd .. && cdk deploy && cd ..
+fi
 
-# deploy backend stack
-cd cdk_front && npm install && cdk deploy && cd ..
+if [[ "$DEPLOY_TYPE" == "f" ]]; then
+    # install react app deps
+    npm install
+
+    # grab backend API endpoint using AWS CLI and build react app
+    API_ENDPOINT=$(
+        aws cloudformation describe-stacks \
+            --stack-name FinanceDashServerStack \
+            --query "Stacks[0].Outputs[?ExportName=='FinanceDashAPIEndpoint'].OutputValue" \
+            --output text
+    )
+    REACT_APP_FINANCE_DASH_API_ENDPOINT=$API_ENDPOINT npm run build
+
+    # deploy frontend stack
+    cd cdk_front && npm install && cdk deploy && cd ..
+fi
