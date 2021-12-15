d719eaf4fd104153e3b16f3a1e9151195447b7d4
robbiejdunn <robbiejdunn94@gmail.com>
2021-12-15 13:55:37 +0000

    get holding pg test

--- cdk_back/lambdas/get-holding/app.js
+++ cdk_back/lambdas/get-holding/app.js
@@ -10,69 +10,45 @@ exports.handler = async (event, context) => {
     }
     try {
         console.log('Received event:', JSON.stringify(event, null, 2));
+        const holdingId = event.multiValueQueryStringParameters.id[0];
 
         const client = new Client();
         await client.connect();
 
         // Get holding joined by ticker
         const getHoldingQuery = `
-            SELECT * FROM get_holding_view WHERE holding_id='${event.multiValueQueryStringParameters.id[0]}'
+            SELECT * FROM get_holding_view WHERE holding_id='${holdingId}'
         `;
         const getHoldingResp = await client.query(getHoldingQuery);
         console.log(getHoldingResp);
+        if (getHoldingResp.rowCount != 1) {
+            throw `Expected 1 row returned found ${getHoldingResp.rowCount}`;
+        }
+
+        // Get transactions for the holding
+        const getTransactionsForHoldingQuery = `
+            SELECT * FROM transactions WHERE holding_id='${holdingId}'
+        `;
+        const getTransactionsForHoldingRes = await client.query(getTransactionsForHoldingQuery);
+        console.log(getTransactionsForHoldingRes);
+
+        // Get ticker prices for the associated ticker id
+        const holdingTickerId = getHoldingResp.rows[0].ticker_id;
+        const getTickerPricesForHoldingQuery = `
+            SELECT * FROM ticker_prices WHERE ticker_id='${holdingTickerId}'
+        `;
+        const getTickerPricesForHoldingRes = await client.query(getTickerPricesForHoldingQuery);
+        console.log(getTickerPricesForHoldingRes);
 
         await client.end();
-        // let params = {
-        //     TableName: HoldingsTableName,
-        //     Key: {
-        //         id: {
-        //             S: event.multiValueQueryStringParameters.id[0]
-        //         }
-        //     }
-        // };
-        // const holdingGetData = await dbClient.getItem(params).promise();
 
-        // params = {
-        //     TableName: TickersTableName,
-        //     Key: {
-        //         id: {
-        //             S: holdingGetData.Item.tickerId.S
-        //         }
-        //     }
-        // }
-        // const tickerGetData = await dbClient.getItem(params).promise();
-
-        // params = {
-        //     TableName: TickerPricesTableName,
-        //     IndexName: 'TickerPricesByTickerIDIndex',
-        //     ExpressionAttributeValues: {
-        //         ':t': {S: holdingGetData.Item.tickerId.S}
-        //     },
-        //     KeyConditionExpression: 'tickerId = :t'
-        // }
-        // const tickerPricesGetData = await dbClient.query(params).promise();
-
-        // params = {
-        //     TableName: TransactionsTableName,
-        //     IndexName: 'TransactionByHoldingIDIndex',
-        //     ExpressionAttributeValues: {
-        //         ':h': {S: holdingGetData.Item.id.S}
-        //     },
-        //     KeyConditionExpression: 'holdingId = :h'
-        // }
-        // const transactionsGetData = await dbClient.query(params).promise();
-        
-        // const responseData = {
-        //     'holding': holdingGetData,
-        //     'ticker': tickerGetData,
-        //     'tickerPrices': tickerPricesGetData,
-        //     'transactions': transactionsGetData
-        // }
-
+        const responseData = {
+            holding: getHoldingResp.rows[0],
+            tickerPrices: getTickerPricesForHoldingRes.rows,
+            transactions: getTransactionsForHoldingRes.rows,
+        }
         response.statusCode = 200;
-        // response.body = JSON.stringify(responseData);
-        response.body = JSON.stringify({test: 'hi'})
-
+        response.body = JSON.stringify(responseData);
     } catch (err) {
         console.log(err);
         response.statusCode = 500;
