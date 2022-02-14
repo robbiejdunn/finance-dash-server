5cc2f4db05b245095c1a91c177cb77bbab33b4a4
robbiejdunn <robbiejdunn94@gmail.com>
2022-02-14 16:20:42 +0000

    remove unused imports

--- src/components/CreateTransactionDialog.js
+++ src/components/CreateTransactionDialog.js
@@ -4,14 +4,11 @@ import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
 import makeStyles from '@mui/styles/makeStyles';
-import { FilledInput, FormControlLabel, FormLabel, IconButton, Radio, RadioGroup, Tooltip } from '@mui/material';
+import { FilledInput, FormControlLabel, IconButton, Radio, RadioGroup, Tooltip } from '@mui/material';
 import axios from 'axios';
 import AddIcon from '@mui/icons-material/Add';
 
